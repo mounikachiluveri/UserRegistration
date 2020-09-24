b62f420b994def2b428d11e097cbe4215344a279
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-09-25 01:25:16 +0530

    Added:Test Case given First Name should Proper Return True

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,22 @@
+import com.bridgelabs.service.UserRegistrationValidator;
+import com.bridgelabs.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserServiceTest
+{
+    UserRegistrationValidator registration;
+
+    @Before
+    public void setUp() throws Exception {
+        UserService userService =new UserService();
+    }
+
+    @Test
+    public void givenFirstName_whenProper_ReturnTrue() {
+        UserRegistrationValidator registration=new UserRegistrationValidator();
+        boolean result = registration.validateInput("Mounika",UserRegistrationValidator.ValidatePattern.NAME);
+        Assert.assertTrue(result);
+    }
+}
